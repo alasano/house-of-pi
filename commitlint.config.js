ab518be53f7713cdedb3d865e57743a8a214c439
Aljosa Asanovic <aljosa.a@gmail.com>
2026-06-20 22:01:01 -0400

    ops(release): customize release please metadata

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,5 +2,24 @@ export default {
   extends: ['@commitlint/config-conventional'],
   rules: {
     'scope-empty': [2, 'never'],
+    'type-enum': [
+      2,
+      'always',
+      [
+        'build',
+        'chore',
+        'ci',
+        'deps',
+        'docs',
+        'feat',
+        'fix',
+        'ops',
+        'perf',
+        'refactor',
+        'revert',
+        'style',
+        'test',
+      ],
+    ],
   },
 };
